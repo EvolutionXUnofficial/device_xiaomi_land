6292700d8dff0b2b18b3fb30cf26651f1df447cd
AlokRonaldo <sabkuchmilega634@gmail.com>
2021-07-25 14:56:44 +0530

    land: Setup vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+rm -rf hardware/qcom-caf/msm8996/audio
+rm -rf hardware/qcom-caf/msm8996/display
+rm -rf hardware/qcom-caf/msm8996/media
+git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_audio hardware/qcom-caf/msm8996/audio
+git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_display hardware/qcom-caf/msm8996/display
+git clone --depth=1 https://github.com/PixelExperience/hardware_qcom-caf_msm8996-r_media hardware/qcom-caf/msm8996/media
+
